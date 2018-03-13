3797bef46c7e73acd82d84484ac9be2cb93dec9b
j-ornelas <johnnyoh1988@gmail.com>
2018-03-13 10:35:05 -0700

    Final commit

--- server.js
+++ server.js
@@ -3,71 +3,84 @@ const morgan = require('morgan');
 const proxy = require('express-http-proxy');
 const path = require('path');
 const app = express();
-const $ = require('jquery')
 const bodyParser = require('body-parser')
 
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
 
-
-app.use('/highlights/reviews/:id', proxy('http://localhost:3003/highlights/reviews/:id', {
+app.use('/title-bar/restaurant/:id', proxy('http://52.8.109.246/title-bar/restaurant/:id', {
   proxyReqPathResolver: function(req) {
-    return `http://localhost:3003/highlights/reviews/` + req.params.id;
+    return `http://52.8.109.246/title-bar/restaurant/` + req.params.id;
   }
 }));
 
-app.use('/reviews/reviews/:id', proxy('http://localhost:3004/reviews/reviews/:id', {
+app.use('/highlights/reviews/:id', proxy('http://54.241.166.39/highlights/reviews/:id', {
   proxyReqPathResolver: function(req) {
-    return `http://localhost:3004/reviews/reviews/` + req.params.id;
+    return `http://54.241.166.39/highlights/reviews/` + req.params.id;
   }
 }));
 
-app.use('/reviews/user/:id', proxy('http://localhost:3004/reviews/user/:id', {
+app.use('/highlights/photos/:id', proxy('http://54.241.166.39/highlights/photos/:id', {
   proxyReqPathResolver: function(req) {
-    return `http://localhost:3004/reviews/user/` + req.params.id;
+    return `http://54.241.166.39/highlights/photos/` + req.params.id;
   }
 }));
 
-app.use('/highlights/photos/:id', proxy('http://localhost:3003/highlights/photos/:id', {
+app.use('/reviews/reviews/:id', proxy('http://13.57.136.163/reviews/reviews/:id', {
   proxyReqPathResolver: function(req) {
-    return `http://localhost:3003/highlights/photos/` + req.params.id;
+    return `http://13.57.136.163/reviews/reviews/` + req.params.id;
+  }
+}));
+
+app.use('/reviews/user/:id', proxy('http://13.57.136.163/reviews/user/:id', {
+  proxyReqPathResolver: function(req) {
+    return `http://13.57.136.163/reviews/user/` + req.params.id;
   }
 }));
 
 app.use(
   "/sidebar/business/:id",
-  proxy("http://localhost:3002/sidebar/business/:id", {
+  proxy("http://13.56.34.255/sidebar/business/:id", {
     proxyReqPathResolver: function(req) {
-      return "http://localhost:3002/sidebar/business/" + req.params.id;
+      return "http://52.53.200.182/sidebar/business/" + req.params.id;
     }
   })
 );
 app.use(
   "/sidebar/postalCode/:code",
-  proxy("http://localhost:3002/sidebar/postalCode/:code", {
+  proxy("http://13.56.34.255/sidebar/postalCode/:code", {
     proxyReqPathResolver: function(req) {
-      return "http://localhost:3002/sidebar/postalCode/" + req.params.code;
+      return "http://52.53.200.182/sidebar/postalCode/" + req.params.code;
     }
   })
 );
 app.use(
   "/sidebar/businessTips/:id",
-  proxy("http://localhost:3002/sidebar/businessTips/:id", {
+  proxy("http://13.56.34.255/sidebar/businessTips/:id", {
     proxyReqPathResolver: function(req) {
-      return "http://localhost:3002/sidebar/businessTips/" + req.params.id;
+      return "http://52.53.200.182/sidebar/businessTips/" + req.params.id;
     }
   })
 );
 
-app.get('/map-and-images/business/:id', proxy('http://localhost:3001/map-and-images/business/:id', {
+app.use(
+  "/sidebar/photos/:id",
+  proxy("http://13.56.34.255/sidebar/photos/:id", {
+    proxyReqPathResolver: function(req) {
+      return "http://52.53.200.182/sidebar/photos/" + req.params.id;
+    }
+  })
+);
+
+app.get('/map-and-images/business/:id', proxy('http://34.216.201.147/map-and-images/business/:id', {
   proxyReqPathResolver: function(req) {
-    return 'http://localhost:3001/map-and-images/business/' + req.params.id;
+    return 'http://34.216.201.147/map-and-images/business/' + req.params.id;
   }
 }));
 
-app.get('/map-and-images/business/:id/photos', proxy('http://localhost:3001/map-and-images/business/:id/photos', {
+app.get('/map-and-images/business/:id/photos', proxy('http://34.216.201.147/map-and-images/business/:id/photos', {
   proxyReqPathResolver: function(req) {
-    return 'http://localhost:3001/map-and-images/business/' + req.params.id + '/photos';
+    return 'http://34.216.201.147/map-and-images/business/' + req.params.id + '/photos';
   }
 }));
 
